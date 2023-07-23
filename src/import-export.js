7ccd40da88a929b20483a49f223dbb59bbecd251
Hannah Kolbeck <hskolbeck@gmail.com>
2023-07-22 23:12:27 -0700

    Putting aside import

--- src/import-export.js
+++ src/import-export.js
@@ -1,6 +1,3 @@
-
-
-
 function exportArchive() {
     const archive = getExportableDescriptions()
     const dataUrl = `data:application/json;charset=utf-8,${encodeURIComponent(archive)}`
@@ -22,11 +19,59 @@ function openImport() {
 }
 
 function importArchive(file) {
+    readJsonFile(file).then(json => importDescriptions(json)).catch(e => {
+        console.log(e)
+        alert("Failed to import JSON archive")
+    })
+}
 
+function importMastodonTgz() {
 
 }
 
-function importMastodon() {
+function importMastodonJson(jsonFile, sampleUrl, reportPct) {
+    const host = new URL(sampleUrl).host
+    readJsonFile(jsonFile).then(async mastoArchive => {
+        const items = mastoArchive.orderedItems
+        let noAlt = 0
+        let toFetch = []
+        for (let [note] of items) {
+            if (note.object && note.object.attachment) {
+                for (let attach of note.object.attachment) {
+                    if (attach.type === "Document"
+                        && attach.mediaType.startsWith("image/")
+                        && attach.url
+                    ) {
+                        if (attach.name) {
+                            toFetch.push({
+                                alt: attach.name,
+                                url: `${host}${attach.url}`
+                            })
+                        } else {
+                            noAlt++
+                        }
+                    }
+                }
+            }
+        }
 
+    }).catch((e) => {
+        console.log(e)
+        alert("Failed to import from Mastodon")
+    })
+}
+
+async function readJsonFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onloadend = async function () {
+            try {
+                resolve(JSON.parse(reader.result))
+            } catch (e) {
+                reject(e)
+            }
+        };
+    })
 }
 
